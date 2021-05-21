7d422337f3884950d645aa69abfd99d934dd4c86
esraa-ismail <esraa.m.ismail@gmail.com>
2021-05-21 12:56:02 +0200

    some edite

--- js/custom.js
+++ js/custom.js
@@ -8,23 +8,18 @@
 		/*  PRELOADER ANIMATION
 		/* ----------------------------------------------------------- */
 	
-		var pageTransitionAnimationDuration = 500
+		var pageTransitionAnimationDuration = 300
 		var preloader = $(".preloader");
 		pageTransition({
 			target: document.querySelector('.page'),
 			delay: 0,
 			duration: pageTransitionAnimationDuration,
 			classActive: 'animated',
-			conditions: function (event, link) {
-				return
-				!/(\#|callto:|tel:|mailto:|:\/\/)/.test(link)
-				&& !event.currentTarget.hasAttribute('data-lightgallery')
-				&& event.currentTarget.getAttribute('href') !== 'javascript:void(0);';
-			},
+			
 			onTransitionStart: function (options) {
 				setTimeout(function () {
 					preloader.removeClass('loaded');
-				}, options.duration * .75);
+				}, options.duration * .75); 
 			},
 			onReady: function () {
 				preloader.addClass('loaded');
@@ -108,31 +103,31 @@
         /*  AJAX CONTACT FORM
         /* ----------------------------------------------------------- */
 
-        $(".contactform").on("submit", function() {
-            $(".output_message").text("Loading...");
+        // $(".contactform").on("submit", function() {
+        //     $(".output_message").text("Loading...");
 
-            var form = $(this);
-            $.ajax({
-                url: form.attr("action"),
-                method: form.attr("method"),
-                data: form.serialize(),
-                success: function(result) {
-                    if (result == "success") {
-                        $(".form-inputs").css("display", "none");
-                        $(".box p").css("display", "none");
-                        $(".contactform").find(".output_message").addClass("success");
-                        $(".output_message").text("Message Sent!");
-                    } else {
-                        $(".tabs-container").css("height", "440px");
+        //     var form = $(this);
+        //     $.ajax({
+        //         url: form.attr("action"),
+        //         method: form.attr("method"),
+        //         data: form.serialize(),
+        //         success: function(result) {
+        //             if (result == "success") {
+        //                 $(".form-inputs").css("display", "none");
+        //                 $(".box p").css("display", "none");
+        //                 $(".contactform").find(".output_message").addClass("success");
+        //                 $(".output_message").text("Message Sent!");
+        //             } else {
+        //                 $(".tabs-container").css("height", "440px");
 
-                        $(".contactform").find(".output_message").addClass("error");
-                        $(".output_message").text("Error Sending!");
-                    }
-                }
-            });
+        //                 $(".contactform").find(".output_message").addClass("error");
+        //                 $(".output_message").text("Error Sending!");
+        //             }
+        //         }
+        //     });
 
-            return false;
-        });
+        //     return false;
+        // });
 
         /* ----------------------------------------------------------- */
         /*  PAGE ANIMATION
